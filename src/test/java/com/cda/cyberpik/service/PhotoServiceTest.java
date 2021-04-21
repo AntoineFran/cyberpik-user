7b46047ec18d3ba552e56fb07f4e2051b8071240
valentin-cochin <cochin.valentin@hotmail.fr>
2021-04-22 00:38:46 +0200

    Add first true Unit test on a service

--- src/test/java/com/cda/cyberpik/service/PhotoServiceTest.java
+++ src/test/java/com/cda/cyberpik/service/PhotoServiceTest.java
@@ -1,38 +1,59 @@
 package com.cda.cyberpik.service;
 
 import com.cda.cyberpik.dao.IRepositoryPhoto;
+import com.cda.cyberpik.dto.PhotoDto;
 import com.cda.cyberpik.entity.Photo;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
+import com.cda.cyberpik.entity.Transformation;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 public class PhotoServiceTest {
+    @Mock
+    private ModelMapper modelMapper;
 
     @Mock
-    IRepositoryPhoto photoDao;
+    private IRepositoryPhoto photoDao;
 
-    @Autowired
     @InjectMocks
-    PhotoService photoService;
+    private PhotoService photoService;
 
-    @BeforeEach
-    void setMockOutput() {
-        when(photoDao.findAll()).thenReturn(new ArrayList<Photo>());
-    }
-
-    @DisplayName("Test Mock photoService + helloRepository")
     @Test
-    void testFindAll() {
-        assertEquals(new ArrayList<Photo>(), photoDao.findAll());
+    void shouldReturnFindAll() {
+        // Context
+        Photo p1 = new Photo();
+        p1.setPhotoId(1L);
+        p1.setTitle("image_1");
+        p1.setPhotoTransformations(new ArrayList<Transformation>());
+
+        Photo p2 = new Photo();
+        p2.setPhotoId(2L);
+        p2.setTitle("image_2");
+        p2.setPhotoTransformations(new ArrayList<Transformation>());
+
+        List<Photo> photos = new ArrayList<>();
+        photos.add(p1);
+        photos.add(p2);
+
+         PhotoDto photoDto = new PhotoDto();
+
+        // Action
+        when(modelMapper.map(any(), any())).thenReturn(photoDto);
+        when(photoDao.findAll()).thenReturn(photos);
+        List<PhotoDto> actual = photoService.getAll();
+
+        // Result
+        assertEquals(2, actual.size());
+        verify(photoDao, times(1)).findAll();
     }
 }
