3340630edb160123fc509e3eaef364efde379273
valentin-cochin <cochin.valentin@hotmail.fr>
2021-04-26 09:16:32 +0200

    Update PhotoController

--- src/main/java/com/cda/cyberpik/controller/PhotoController.java
+++ src/main/java/com/cda/cyberpik/controller/PhotoController.java
@@ -4,7 +4,9 @@ import com.cda.cyberpik.dto.FormatDto;
 import com.cda.cyberpik.dto.PhotoDto;
 import com.cda.cyberpik.dto.user.account.dto.UserAccountDto;
 import com.cda.cyberpik.exception.ServiceException;
+import com.cda.cyberpik.service.FormatService;
 import com.cda.cyberpik.service.PhotoService;
+import com.cda.cyberpik.service.TransformationService;
 import org.apache.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,10 +18,13 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 
 @RestController
-@RequestMapping(path = "/image")
+@RequestMapping(path = "/images")
 public class PhotoController {
     @Autowired
     PhotoService photoService;
+
+    @Autowired
+    FormatService formatService;
 
     @CrossOrigin
     @GetMapping(path = "/{id}", produces = MediaType.IMAGE_JPEG_VALUE)
@@ -33,17 +38,36 @@ public class PhotoController {
                 .body(bytes);
     }
 
+
+
     @CrossOrigin
-    @PostMapping(path = "/upload")
+    @PostMapping(path = "/")
     public ResponseEntity<?> uploadImage(@RequestParam("file") MultipartFile file) throws IOException {
+
+        // TODO: use authentication to get user account id
+
+        //        public ResponseEntity<String> createNewAlert(@RequestBody NoListAlertDTO newALert, Authentication authentication) {
+        //            if (newALert.getBike().getImage().getImageId() == 0) {
+        //                newALert.getBike().setImage(null);
+        //            }
+        //            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        //            System.out.println(((MyUserDetails) userDetails).getId());
+        //            newALert.setPerson(PersonDTO.builder().personId(((MyUserDetails) userDetails).getId()).build());
+        //            this.alertService.add(newALert);
+        //            return ResponseEntity.status(HttpStatus.OK).body("Alert created");
+        //        }
+
         if (file.isEmpty()) {
             return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body("File is empty");
         }
 
         FormatDto format = new FormatDto();
         String extension = FilenameUtils.getExtension(file.getOriginalFilename());
-        format.setFormatId(1L);
-        // format.setName(extension);
+        try {
+            format = formatService.getFormatByName(extension);
+        } catch (ServiceException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Format not supported");
+        }
 
         UserAccountDto userAccount = new UserAccountDto();
         userAccount.setUserAccountId(1L);
