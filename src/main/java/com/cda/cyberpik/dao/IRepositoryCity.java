1895068e7e8d4ae0ad00c1a407ab098c68bb84f3
Antoine FRANCOIS <francois.antoine3@gmail.com>
2021-04-26 09:04:26 +0200

    Delete the city & country entity, dao, dto, service and their connections

--- src/main/java/com/cda/cyberpik/dao/IRepositoryCity.java
+++
@@ -1,6 +0,0 @@
-package com.cda.cyberpik.dao;
-
-import com.cda.cyberpik.entity.City;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IRepositoryCity extends JpaRepository<City, Long> {}
