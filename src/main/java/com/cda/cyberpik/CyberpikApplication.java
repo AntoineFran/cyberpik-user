7321f69e8541cc97b673bbad6d70e8172c7199d1
Antoine FRANCOIS <francois.antoine3@gmail.com>
2021-05-25 16:25:50 +0200

    Add heroku config in pom.xl and CyberpikApplication

--- src/main/java/com/cda/cyberpik/CyberpikApplication.java
+++ src/main/java/com/cda/cyberpik/CyberpikApplication.java
@@ -3,12 +3,15 @@ package com.cda.cyberpik;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
-public class CyberpikApplication {
+public class CyberpikApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(CyberpikApplication.class, args);
@@ -17,6 +20,11 @@ public class CyberpikApplication {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public ServletWebServerFactory servletWebServerFactory() {
+        return new TomcatServletWebServerFactory();
     }
 
     @Bean
